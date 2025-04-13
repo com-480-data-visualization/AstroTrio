92d8757a29e467a57e2311ec8b4508cc3cc17147
Iman Attia <iman.attia@epfl.ch>
2025-04-13 13:53:25 +0200

    adding index.html

--- docs/app.py
+++ docs/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("index.html")
     # return "<p>Hello, World!</p>"
 
 @app.route("/home")
